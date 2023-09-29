bca20ca6b646bd43d8041a53b68df4ca4229a54a
Kean Brown <keanbr@gmail.com>
2023-09-29 14:42:38 -0400

    Initial terraform init

--- main.tf
+++ main.tf
@@ -2,12 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=3.75.0"
     }
   }
 }
 
-resource "azurerm_resource_group" "RGPrimaryBFH" {
-  name     = "bfh"
-  location = "East US"
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
+
+resource "azurerm_resource_group" "bfhrg" {
+  name     = "bfh"
+  location = "East Us"
+}
+
