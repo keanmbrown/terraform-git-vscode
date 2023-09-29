c17c0d3e2aa86189b12087cf4fdd0914b876e9f6
Kean Brown <keanbr@gmail.com>
2023-09-29 15:24:49 -0400

    Update Image to 20.04 LTS

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "azurerm_linux_virtual_machine" "lvm1" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
 
   }
