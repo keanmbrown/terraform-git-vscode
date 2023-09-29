a07d2571198ded90c8734f3fcca512df3b3a47ba
Kean Brown <keanbr@gmail.com>
2023-09-29 15:08:35 -0400

    Changes

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
@@ -18,11 +17,11 @@ resource "azurerm_resource_group" "bfhrg" {
 }
 
 resource "azurerm_virtual_network" "bfhvnet" {
-  name = "vnet1"
+  name                = "vnet1"
   resource_group_name = azurerm_resource_group.bfhrg.name
-  location = azurerm_resource_group.bfhrg.name
-  address_space = ["10.50.0.0/16"]
+  location            = azurerm_resource_group.bfhrg.name
+  address_space       = ["10.50.0.0/16"]
 
-  
+
 }
 
